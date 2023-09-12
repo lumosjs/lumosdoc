bf839668c88029f446112df36dcb2308fd2db967
sombo20 <vicentevictorsombo643@gmail.com>
2023-09-11 20:49:57 -0400

    Verify phone

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserView> 
+      <App/>
+    </BrowserView>
+    <MobileView>
+      <h1 className="text-2xl text-center">Abre no computador porfavor...</h1>
+    </MobileView>
   </React.StrictMode>,
 )
