7e8f7bfd063677683c84d7c940fdd91f1d3d1898
Vicente Sombo <64757262+sombo20@users.noreply.github.com>
2023-09-11 22:13:48 -0400

    Update intro.jsx

--- src/pages/docs/intro.jsx
+++ src/pages/docs/intro.jsx
@@ -50,105 +50,105 @@ export default function Intro() {
             O framework segue uma estrutura de pastas bem definida para organizar todos os componentes do seu aplicativo:
           </p>
 
-          <div style={{
-            width:750,
-            height:"100%",
-            borderRadius:8,
-            background:"rgba(10,10,10,0.95)",
-          }}>
+           <div className="flex justify-center items-center h-screen">
+      <div className="w-80">
+        <h1 className="text-2xl font-semibold mb-4">Estrutura de Pastas</h1>
+        
+        {/* Pasta app */}
+        <div className="flex items-center space-x-4">
+          <div className="w-4 h-4 bg-black rounded-full"></div>
+          <p className="text-black">app/</p>
+        </div>
 
-            <div style={{
-              color:"grey",
-              display:"flex",
-              flexDirection:"column",
-              padding:15  
-            }}>
-            <div>
-              <p>
-                app/ : Aqui reside a lógica central do seu aplicativo.
-              </p>
+        {/* Subpastas dentro de app */}
+        <div className="ml-6 space-y-2">
+          <div className="flex items-center space-x-4">
+            <div className="w-3 h-3 bg-black rounded-full"></div>
+            <p className="text-black">controllers/</p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <div className="w-3 h-3 bg-black rounded-full"></div>
+            <p className="text-black">services/</p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <div className="w-3 h-3 bg-black rounded-full"></div>
+            <p className="text-black">middlewares/</p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <div className="w-3 h-3 bg-black rounded-full"></div>
+            <p className="text-black">models/</p>
+          </div>
+        </div>
 
-            <div className="ml-4" style={{color:'grey'}}>
-              <p style={{color:'#fff'}}>
-                /controllers
-              </p>
-              <p style={{color:'#fff'}}>
-                /services
-              </p>
-              <p style={{color:'#fff'}}>
-                /middlewares
-              </p>
-              <p style={{color:'#fff'}}>
-                /models
-              </p>
-            </div>
-            </div>  
+        {/* Pasta routes */}
+        <div className="flex items-center space-x-4 mt-4">
+          <div className="w-4 h-4 bg-black rounded-full"></div>
+          <p className="text-black">routes/</p>
+        </div>
 
+        {/* Arquivo api.js dentro de routes */}
+        <div className="ml-6">
+          <div className="flex items-center space-x-4">
+            <div className="w-3 h-3 bg-black rounded-full"></div>
+            <p className="text-black">api.js</p>
+          </div>
+        </div>
 
-            <div className="mt-4">
-              <p>
-                routes/ : Aqui são definidas as rotas do aplicativo
-              </p>
+        {/* Pasta public */}
+        <div className="flex items-center space-x-4 mt-4">
+          <div className="w-4 h-4 bg-black rounded-full"></div>
+          <p className="text-black">public/</p>
+        </div>
 
-            <div className="ml-4" style={{color:'grey'}}>
-              <p style={{color:'#fff'}}>
-                api.js : Definições das rotas da API.
-              </p>
-            </div>
-            </div>  
+        {/* Pasta log */}
+        <div className="flex items-center space-x-4 mt-4">
+          <div className="w-4 h-4 bg-black rounded-full"></div>
+          <p className="text-black">log/</p>
+        </div>
 
-            <div className="mt-4">
-              <p>
-                public/ : Pasta pública do servidor web para arquivos acessíveis publicamente
-              </p>
+        {/* Pasta test */}
+        <div className="flex items-center space-x-4 mt-4">
+          <div className="w-4 h-4 bg-black rounded-full"></div>
+          <p className="text-black">test/</p>
+        </div>
 
-            </div>  
+        {/* Pasta lib */}
+        <div className="flex items-center space-x-4 mt-4">
+          <div className="w-4 h-4 bg-black rounded-full"></div>
+          <p className="text-black">lib/</p>
+        </div>
 
-              <div>
-              <p>
-                log/ : Armazena arquivos gerados pelo aplicativo, como logs de erros etc.
-              </p>
-            </div>  
+        {/* Subpastas dentro de lib */}
+        <div className="ml-6 space-y-2">
+          <div className="flex items-center space-x-4">
+            <div className="w-3 h-3 bg-black rounded-full"></div>
+            <p className="text-black">core/</p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <div className="w-3 h-3 bg-black rounded-full"></div>
+            <p className="text-black">helpers/</p>
+          </div>
+          <div className="flex items-center space-x-4">
+            <div className="w-3 h-3 bg-black rounded-full"></div>
+            <p className="text-black">utils/</p>
+          </div>
+        </div>
 
-             <div>
-              <p>
-                test/ : Contém testes automatizados para garantir a robustez do seu aplicativo.
-              </p>
-            </div>  
+        {/* Pasta config */}
+        <div className="flex items-center space-x-4 mt-4">
+          <div className="w-4 h-4 bg-black rounded-full"></div>
+          <p className="text-black">config/</p>
+        </div>
 
-
-            <div className="mt-4">
-              <p>
-                config/ : Contém arquivos de configuração para diferentes componentes do framework.
-              </p>
-
-            <div className="ml-4" style={{color:'grey'}}>
-              <p style={{color:'#fff'}}> 
-              headers.js : Configurações para o tratamento dos Headers.
-              </p>
-            </div>
-            </div> 
-
-            <div className="mt-4">
-              <p>
-                lib/ : Bibliotecas e utilitários específicos do framework.
-              </p>
-
-            <div className="ml-4" style={{color:'grey'}}>
-              <p style={{color:'#fff'}}> 
-                core/ : Componentes principais do aplicativo.
-              </p>
-               <p style={{color:'#fff'}}> 
-                helpers/ : Funções auxiliares para facilitar o desenvolvimento.
-              </p>
-               <p style={{color:'#fff'}}> 
-                utils/ : Utilitários gerais.
-              </p>
-            </div>
-            </div> 
-
-            </div>  
-          </div>  
+        {/* Arquivo headers.js dentro de config */}
+        <div className="ml-6">
+          <div className="flex items-center space-x-4">
+            <div className="w-3 h-3 bg-black rounded-full"></div>
+            <p className="text-black">headers.js</p>
+          </div>
+        </div>
+      </div>
+    </div>
 
         <h4 className="text-2xl mb-8 mt-6"> Estado Atual do Projeto</h4>
         <p className="mb-4">
